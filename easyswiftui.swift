7059e0e00d4ac03bf0bc4ee7dc0954902d57e128
Benjamin Scanlan <ben.scanlan@yahoo.com>
2023-05-02 18:04:56 -0700

    Update easyswiftui.swift

---
+++ easyswiftui.swift
@@ -0,0 +1,11 @@
+# Crypto for College Students
+
+## esscoin crypto plus swift ui
+
+- Crypto for College Students.
+
+- The movie Social Network (2008) was a psychological operation to get people in early stage start ups to get paranoid about ownership over currently non existent revenue generating business. FYP
+
+# esscoin goal
+- swift app for easy transfers
+- fork of popular crypto repo for our own use
